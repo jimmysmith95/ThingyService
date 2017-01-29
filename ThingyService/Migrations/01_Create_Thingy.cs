30bdc633a8dfdcd3a051a20b9531f20147e284d7
James Smith <James Smith>
2017-01-29 15:33:19 -0500

    intial commit; added migration

---
+++ ThingyService/Migrations/01_Create_Thingy.cs
@@ -0,0 +1,23 @@
+﻿using SimpleMigrations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThingyService.Migrations
+{
+    [Migration(1)]
+    class Migration01 : Migration
+    {
+        public override void Up()
+        {
+            Execute("CREATE TABLE Thingy (Id int Identity(1,1) PRIMARY KEY, Name varchar(255) NOT NULL)");
+        }
+
+        public override void Down()
+        {
+            Execute("DROP TABLE Thingy");
+        }
+    }
+}
--- ThingyService/Program.cs
+++ ThingyService/Program.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using SimpleMigrations;
+using SimpleMigrations.DatabaseProvider;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +13,14 @@ namespace ThingyService
     {
         static void Main(string[] args)
         {
+            var migrationsAssembly = typeof(Program).Assembly;
+            using (var connection = new SqlConnection("Data Source=localhost\\SQLEXPRESS;Database=Thingy;Integrated Security=SSPI;"))
+            {
+                var databaseProvider = new MssqlDatabaseProvider(connection);
+                var migrator = new SimpleMigrator(migrationsAssembly, databaseProvider);
+                migrator.Load();
+                migrator.MigrateToLatest();
+            }
         }
     }
 }
